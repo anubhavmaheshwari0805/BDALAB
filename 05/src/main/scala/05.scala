a7c64fb39ea938cd8fbdd03072b6a0406a325ed5
Anubhav Maheshwari <anubhavmaheshwari0805@gmail.com>
2021-04-25 01:39:15 +0530

    5th added

---
+++ 05/src/main/scala/05.scala
@@ -0,0 +1,26 @@
+/*object WordCount {
+    def main(args:Array[String]) : Unit = {
+        val wordlist = List("name", "char", "str", "str", "str", "kl")
+        var wordCount = scala.collection.mutable.Map[String,Int] ()
+        for (word<-wordlist)
+            wordCount(word) = if(wordCount.contains(word))
+                wordCount(word)+1
+            else 1
+        println(wordCount)
+    }
+}*/
+import scala.io.Source
+object WordCount {
+    def main(args:Array[String]) : Unit = {
+        //val wordlist = List("name", "char", "str", "str", "str", "kl")
+        val filename = args(0)
+        var wordCount = scala.collection.mutable.Map[String,Int] ()
+        for (line<-Source.fromFile(filename).getLines)
+            for(word<-line.split(" "))
+                wordCount(word) = if(wordCount.contains(word))
+                                    wordCount(word)+1
+                                else 
+                                    1
+        println(wordCount)
+    }
+}
